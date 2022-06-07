bf3434ed30a967886a2faf6f5b7cb8acfb9ca9e0
Abel <abelmesfin555@gmail.com>
2022-06-07 20:29:33 +0300

    commited

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,25 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
+					if (i = 2 && j == 4)
+					{
+						break;
+					}
+					else
+					{
 					_putchar('0' + i);
 					_putchar('0' + j);
 					_putchar(':');
 					_putchar('0' + k);
 					_putchar('0' + l);
 					_putchar('\n');
+					}
 				}
 			}
 		}
