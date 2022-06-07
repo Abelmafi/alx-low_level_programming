7863cb02b51e80e39a12367d0779b52d1c333521
Abel <abelmesfin555@gmail.com>
2022-06-07 15:24:05 +0300

    commited

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print_alphabet
+ *
+ * Return: nething
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		char b = 'a';
+
+		while (b <= 'z')
+		{
+			_putchar(b);
+			b++;
+		}
+		a++;
+		_putchar('\n');
+	}
+}
+
