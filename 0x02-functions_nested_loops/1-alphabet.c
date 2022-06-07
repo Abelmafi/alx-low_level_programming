dae1b80163a4ed670282f20c853af755e25254b0
Abel <abelmesfin555@gmail.com>
2022-06-07 13:39:12 +0300

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+int print_alphabet(void);
+#endif
