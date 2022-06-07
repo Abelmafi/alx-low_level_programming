f16525b85d0115433fea65287b89935dccc3926f
Abel <abelmesfin555@gmail.com>
2022-06-07 18:42:59 +0300

    commited

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * _abs - give absolute value
+ *
+ * @c: number
+ *
+ * Return: absolute value.
+ */
+
+int _abs(int c)
+{
+	int b = abs(c);
+
+	return (b);
+}
