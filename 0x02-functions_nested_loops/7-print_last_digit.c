5957fe38a6863639cb3c3a1262bcf49647a529a1
Abel <abelmesfin555@gmail.com>
2022-06-07 19:22:35 +0300

    commited

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - check the code
+ *
+ *@b: absolute value
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int b)
+{
+	int r;
+
+	if (b < 0)
+	{
+		b = b * -1;
+		r = b % 10;
+	}
+	else if (b > 0)
+	{
+		r = b % 10;
+	}
+	else
+	{
+		r = 0;
+	}
+	return (r);
+}
