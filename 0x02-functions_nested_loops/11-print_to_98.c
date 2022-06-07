1a5d317a66d6d97417cb24c999d5945b10511279
Abel <abelmesfin555@gmail.com>
2022-06-08 00:04:34 +0300

    commited

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - main
+ *
+ * @n: int
+ *
+ * Return: value
+ */
+void print_to_98(int n)
+{
+	while ((n < 98) || (n > 98))
+	{
+		if (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		else if (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("%d\n", 98);
+}
