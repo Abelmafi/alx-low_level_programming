d550c050684b826685f4cadd92e1f9a0f7455682
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-06-07 20:44:33 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,19 +10,19 @@
 void jack_bauer(void)
 {
 	int i, j, k, l;
-	bool stop = false;
+	int d = 0;;
 
-	for (i = 0; (i <= 2) && !stop; i++)
+	for (i = 0; i <= 2 && d = 0; i++)
 	{
-		for (j = 0; (j <= 9)&& !stop; j++)
+		for (j = 0; j <= 9 && d = 0; j++)
 		{
-			for (k = 0; (k <= 5) && !stop; k++)
+			for (k = 0; k <= 5 && d = 0; k++)
 			{
-				for (l = 0; (l <= 9) && !stop; l++)
+				for (l = 0; l <= 9 && d = 0; l++)
 				{
 					if (i == 2 && j == 4)
 					{
-						stop = true;
+						d = 1;
 					}
 					_putchar('0' + i);
 					_putchar('0' + j);
