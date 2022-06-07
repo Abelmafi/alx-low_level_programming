51aff856f81dff51998522c8dd79868c9205fb46
Abel <abelmesfin555@gmail.com>
2022-06-08 00:53:54 +0300

    commited

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - check the code.
+ *
+ * @n: intiger
+ *
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+	int i, j, a;
+
+	if (n > 15 || n < 0)
+	{
+		printf("\n");
+	}
+	else
+	{
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			a = i * j;
+
+			if (j == 0)
+			{
+				printf("%d", a);
+			}
+			else
+			{
+			if (a < 10)
+			{
+				printf(",   %d", a);
+			}
+			else if (a >= 10 && a < 100)
+			{
+				printf(",  %d", a);
+			}
+			else
+			{
+				printf(", %d", a);
+			}
+			}
+		}
+		printf("\n");
+	}
+	}
+}
