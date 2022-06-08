fb57b03b30f0cfec50ca3b1d0bbaae5cd2dfef99
Abel <abelmesfin555@gmail.com>
2022-06-08 13:01:32 +0300

    commited

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	long int t1 = 1, t2 = 2, a = 0;
+	long int nextTerm = t1 + t2;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (nextTerm >= 4000000)
+		{
+			break;
+		}
+		else
+		{
+		if (nextTerm % 2 == 0)
+		{
+			a = a + nextTerm;
+		}
+		else
+		{};
+		}
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	printf("%ld\n", a);
+	return (0);
+}
