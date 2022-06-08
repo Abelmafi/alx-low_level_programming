b977ec3112947936042a297217706fc580464536
Abel <abelmesfin555@gmail.com>
2022-06-08 10:15:42 +0300

    updated

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, c = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		int a = i % 3;
+		int b = i % 5;
+
+		if (a == 0 || b == 0)
+		{
+			c = c + i;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%d\n", c);
+}
