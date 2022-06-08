7aa2ec82c3bee0a89f24efc147f1a657878b77b8
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-06-08 10:57:58 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	int i;
-	long long int t1 = 1, t2 = 2;
-	long long int nextTerm = t1 + t2;
+	long int t1 = 1, t2 = 2;
+	long int nextTerm = t1 + t2;
 
 
-	printf("%lld, %lld, ", t1, t2);
+	printf("%ld, %ld, ", t1, t2);
 	for (i = 3; i <= 50; ++i)
 	{
-		printf("%lld, ", nextTerm);
+		printf("%ld, ", nextTerm);
 		t1 = t2;
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
