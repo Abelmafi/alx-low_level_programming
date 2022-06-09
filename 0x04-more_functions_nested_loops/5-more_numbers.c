6056f76738ad336c2e3f72a0c2b4dc4208351a88
Abel <abelmesfin555@gmail.com>
2022-06-10 01:17:56 +0300

    commited

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print_numbers
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('0' + (j / 10));
+				_putchar('0' + (j % 10));
+			}
+			else
+				_putchar('0' + j);
+		}
+		_putchar('\n');
+	}
+}
