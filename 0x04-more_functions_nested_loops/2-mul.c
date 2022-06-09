74c00febca0be058b666ce3afa4000cc8c0e7139
Abel <abelmesfin555@gmail.com>
2022-06-10 00:10:52 +0300

    commited

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ *
+ * @a: intiger
+ * @b: intiger
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
