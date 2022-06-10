03a32aec9c312e2f59c9f75647bbfde0ed9733f8
Abel <abelmesfin555@gmail.com>
2022-06-10 03:03:45 +0300

    commited

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - starting function
+ *
+ * @size: intiger of a function
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = (size - 1); j >= 0; j--)
+		{
+			if (j <= i)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
