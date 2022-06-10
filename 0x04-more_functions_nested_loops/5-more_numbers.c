a9085b24976e3d9fee2c959ceff5bd729e9d7500
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-06-10 03:13:58 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
- * more_numbers - print_numbers
+ * more_numbers - starting function
  *
  */
 void more_numbers(void)
 {
-	int i, j;
-	
-	for (i = 0; i <= 9; i++)
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			if (j > 9)
-			{
+			if ((j / 10) > 0)
 				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
-			}
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
