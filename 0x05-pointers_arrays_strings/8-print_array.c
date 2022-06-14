1dd4a982ad30756c6fdb6206391c2bfbe08cb100
Abel <abelmesfin555@gmail.com>
2022-06-14 03:07:51 +0300

    commited"

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print array variable
+ * @a: string
+ * @n: intiger
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i, j;
+
+	j = n - 1;
+	i = 0;
+	while (i < j)
+	{
+		printf("%d, ", *(a + i));
+		i++;
+	}
+	printf("%d", *(a + j));
+	printf("\n");
+}
