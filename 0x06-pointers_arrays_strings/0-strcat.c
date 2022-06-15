456ff7ae3ee11b74befbac7845ce1c0b6aa98607
Abel <abelmesfin555@gmail.com>
2022-06-15 11:40:18 +0300

    commited

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - check the code
+ * @dest: destination array
+ * @src: sourse array
+ * Return: Always destination array.
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
