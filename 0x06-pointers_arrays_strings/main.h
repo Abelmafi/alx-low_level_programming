f30f7a0016a01db0ba95cbf94d0cddd22fd8d2e4
Abel <abelmesfin555@gmail.com>
2022-06-15 15:56:30 +0300

    commited

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_array(int *a, int n);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
