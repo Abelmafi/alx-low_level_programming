a693693fe63ea9fdd5fc34ef155ab86f0d088a0a
Abel <abelmesfin555@gmail.com>
2022-06-15 14:00:47 +0300

    commited'

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - check the code
+ * @s1: destination array
+ * @s2: sourse array
+ * Return: Always destination array.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
