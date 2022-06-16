508d60763d2556a1d741356d7dd6c27eacb2eb1a
Abel <abelmesfin555@gmail.com>
2022-06-16 11:12:31 +0300

    commited

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * string_toupper - check the code
+ * cap_string - check the code
  * @str: string to be changed
  * Return: char.
  */
@@ -13,13 +13,37 @@ char *cap_string(char *str)
 
 	while (str[j])
 	{
-		if (str[j] == '\n' || str[j] == '	' || str[j] == ' ' || str[j] == ',' || str[j] == ';'|| str[j] == '.' || str[j] == '!' || str[j] == '?' || str[j] == '"' || str[j] == '(' || str[j] == ')' || str[j] == '{' || str[j] == '}')
-		{
-			if(str[j + 1] >= 'a' && str[j + 1] <= 'z')
+		if (str[j] == '\n' || str[j] == '	' || str[j] == ' ' || str[j] == ',')
 			{
-		str[j + 1] -= 'a' - 'A';
-		}
-		}
+			if (str[j + 1] >= 'a' && str[j + 1] <= 'z')
+			{
+				str[j + 1] -= 'a' - 'A';
+			}
+			}
+		else if (str[j] == ';')
+			{
+			if (str[j + 1] >= 'a' && str[j + 1] <= 'z')
+			{
+				str[j + 1] -= 'a' - 'A';
+			}
+			}
+		else if (str[j] == '.' || str[j] == '!' || str[j] == '?' || str[j] == '"')
+			{
+			if (str[j + 1] >= 'a' && str[j + 1] <= 'z')
+			{
+				str[j + 1] -= 'a' - 'A';
+			}
+			}
+		else if (str[j] == '(' || str[j] == ')' || str[j] == '{' || str[j] == '}')
+			{
+			if (str[j + 1] >= 'a' && str[j + 1] <= 'z')
+			{
+				str[j + 1] -= 'a' - 'A';
+			}
+			}
+		else
+		{}
 		j++;
 	}
+	return (str);
 }
