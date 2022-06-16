d0b1810486aeeb5c85883f9e0602cc0da5a3b18b
Abel <abelmesfin555@gmail.com>
2022-06-16 17:24:59 +0300

    commited

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,30 +10,14 @@
 
 void print_number(int n)
 {
-	int j, e, a, b = 0;
-
-	a = n;
-	e = n;
-	while (a <= 10)
+	if (n < 0)
 	{
-		a = a / 10;
-		b++;
+		_putchar('-');
+		n = -n;
 	}
-
-	if (n >= 10)
-	{
-		for (j = b; j > 0; j--)
-		{
-			int i = pow(10, j);
-			int d = n / i;
-
-			_putchar(d);
-			n = n / 10;
-		}
-		_putchar(e % 10);
-	}
-	else
-	{
+	if (n == 0)
 		_putchar(n);
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
