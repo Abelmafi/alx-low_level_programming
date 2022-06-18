61ea378048ebfb536ef180f15d4e968156e926bb
Abel <abelmesfin555@gmail.com>
2022-06-18 10:38:50 +0300

    commited

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * infinite_add - add two string of numbers
+ * @n1: frist number string
+ * @n2: second number string
+ * @r: result buffer
+ * @size_r: result buffer size
+ * Return: char.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int l = 0;
+	int a = strlen(n1);
+	int b = strlen(n2);
+	int size;
+
+	size = ((a > b) ? a : b);
+	if ((a + 2) > size_r || (b + 2) > size_r)
+	{
+		return (0);
+	}
+	else
+	{
+		while (a > 0 || b > 0)
+		{
+			int f = (int)((a > 0) ? (n1[a - 1] - '0') : 0);
+			int g = (int)((b > 0) ? (n2[b - 1] - '0') : 0);
+			int n = f + g + l;
+			int m = n % 10;
+
+			if (n > 9)
+			{
+				r[size] = (char)(m  + '0');
+				l = 1;
+			}
+			else
+			{
+				r[size] = (char)(n + '0');
+				l = 0;
+			}
+			size--;
+			a--;
+			b--;
+		}
+		if (l == 1)
+		{
+			r[0] = 1 + '0';
+		}
+	}
+	return (r);
+}
