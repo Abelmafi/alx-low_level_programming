44f08722af89cb6e7785a9f828ed9436bc4a1fe0
abel mesfin <abelmesfin555@gmail.com>
2022-06-24 14:38:50 +0300

    commited

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: number of argument
+ * @argv: argument variable
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+		
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
+
