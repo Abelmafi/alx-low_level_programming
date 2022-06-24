2f54ed7f20ed272c4202763390e869a30370f8e3
abel mesfin <abelmesfin555@gmail.com>
2022-06-24 15:06:34 +0300

    commited

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of argument
+ * @argv: argument variable
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int j;
+
+	(void) argc;
+	if (argv[1] == NULL || argv[2] == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", j);
+	}
+}
