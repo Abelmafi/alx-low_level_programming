6855986f11185f7da680e515435a68763606bfd7
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-06-28 17:01:43 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,9 +23,9 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
-		free(s);
 		for (i = 0; i < width; i++)
 			free(s[i]);
+		free(s);
 	}
 	for (i = 0; i < width; i++)
 	{
