5b0506e314c809656933aa7414b9199d00f8f911
abelmesfin <abelmesfin555@gmail.com>
2022-07-01 17:34:49 +0300

    commited

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *s;
 	unsigned int i = 0;
 
-	s = malloc(nmemb * size);
+	s = malloc(nmemb * size + 1);
 	if (s == NULL || nmemb == 0 || size == 0)
 		return (NULL);
 	while (i < nmemb)
@@ -21,6 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		s[i] = 0;
 		i++;
 	}
+	s[i] = '\0';
 	return (s);
 }
 
