b96626d8ef573b076e1be806066bc421a14bb4aa
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-07-01 18:08:49 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,8 +30,7 @@ char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *s;
-	unsigned int i = 0;
-
+	
 	s = malloc(nmemb * size);
 	if (s == NULL || nmemb == 0 || size == 0)
 		return (NULL);
