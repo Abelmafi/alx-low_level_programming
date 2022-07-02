c988a5525176907ab16356fb4fa518debd306231
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-07-02 11:00:14 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int size, i;
 
 	(old_size < new_size) ? (size = old_size) : (size = new_size);
-	printf("%d\n", size);
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
