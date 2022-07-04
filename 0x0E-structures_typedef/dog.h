6bf9ef1cf5a02f0832ea2c174664f69d56f1df1e
abelmesfin <abelmesfin555@gmail.com>
2022-07-04 23:54:54 +0300

    commited

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - list doges
+ * @name: list name
+ * @age: age
+ * @owner: oner name.
+ *
+ * Description: personal info detail
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
