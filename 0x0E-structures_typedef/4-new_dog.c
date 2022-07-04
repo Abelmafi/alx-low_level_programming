5e1208d78a5fb754a19b729311406bd2fe65c6cc
abelmesfin <abelmesfin555@gmail.com>
2022-07-05 01:49:48 +0300

    commited

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,25 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+#include <string.h>
 /**
- * new_dog - check the code
- * @name: name
- * @age: age
- * @owner: owner
- *
- * Return: new structure.
- */
-dog_t *new_dog(char *name, float age, char *owner)
+  * _strdup - returns a pointer to a newly allocated space in memory
+  * contains a copy of the string given as a parameter.
+  * @str: input array
+  * Return: pointer to array
+  */
+char *_strdup(char *str)
 {
-	dog_t *new;
+	char *p;
+	int i, size = 0;
 
-	new = malloc(sizeof(dog_t));
-	if (new == NULL)
-		return ('\0');
-	new->name = name;
-	new->age = age;
-	new->owner = owner;
-	return (new);
+	if (!str)
+		return (0);
+	while (str[size] != '\0')
+	{
+	size++;
+	}
+	p = malloc((size + 1) * sizeof(char));
+	if (p == NULL)
+	return (p);
+	for (i = 0; i < size; i++)
+	{
+	p[i] = str[i];
+	}
+	p[i] = '\0';
+	return (p);
 }
 
+/**
+* new_dog - check the code for Holberton School students.
+* @name: name's dog
+* @age: age's dog
+* @owner: owner's dog
+* Return: new dog pointer
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new1;
+
+	new1 = malloc(sizeof(dog_t));
+	if (new1 == NULL)
+	return (NULL);
+	new1->name = _strdup(name);
+	if (new1->name == 0)
+	free(new1);
+	new1->age = age;
+	new1->owner = _strdup(owner);
+	if (new1->owner == 0)
+	{
+		if (new1->name != 0)
+		free(new1->name);
+		free(new1);
+	}
+return (new1);
+}
