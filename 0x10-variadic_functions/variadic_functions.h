44a0d6d164791da19bceba3956abb11da29839fd
abelmesfin <abelmesfin555@gmail.com>
2022-07-07 21:41:22 +0300

    commited

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARDIADIC_FUNCTION_H
 #define VARDIADIC_FUNCTION_H
 
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
 
