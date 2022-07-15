f0973f2da156c0dc61079ec2c5fec60372e93c4d
abelmesfin <abelmesfin555@gmail.com>
2022-07-16 00:35:31 +0300

    bug fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ int _strlen(const char *st)
  * @str: string to add
  * Return: head
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *aux;
