adc706beb1613851e948b1d2aee0bcdb67519e1f
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-07-19 04:01:00 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (*head);
 	}
-	while (i <= idx)
+	while (i < idx)
 	{
 		current = current->next;
 		if (current == NULL)
