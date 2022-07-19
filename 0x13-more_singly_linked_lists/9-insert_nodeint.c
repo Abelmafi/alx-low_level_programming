ca04cd5c2ae00b4e4abfc52d794dbca7ccae21be
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-07-19 04:05:23 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (!*head || !head)
+	{
+		free(new);
+		return (NULL);
+	}
 	current = *head;
 	if (idx == 0)
 	{
