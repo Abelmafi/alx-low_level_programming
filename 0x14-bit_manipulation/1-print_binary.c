642e1af10170cb5ce4230f8dce24adcb7edd985e
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-21 21:32:59 +0300

    commited

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_binary - print binary of number.
+ * @n: an intiger to be converted to binary.
+ *
+ * Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int i;
+	int k;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+	for (i = 1 << 31; i > 0; i /= 2)
+	{
+		if (n & i)
+		{
+			k = 1;
+		}
+		if (k == 1)
+		{
+			(n & i) ? _putchar('1') : _putchar('0');
+		}
+	}
+	}
+}
+
+
