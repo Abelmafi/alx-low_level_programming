7db870baba68aa600bea83be2f8ce3143572d365
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-21 19:23:53 +0300

    commited

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_to_uint - changes binary number system to desimal
+ * @b: binary string.
+ *
+ * Return: unsigned intiger.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, j, n = 1, k, l = 0;
+	unsigned int sum = 0;
+
+	while (b[i])
+	{
+		i++;
+	}
+	i--;
+	for ( ; i >= 0; i--)
+	{
+		k = b[i] - '0';
+		if (k != 0 && k != 1)
+			return (0);
+		if (k == 1)
+		{
+			for (j = 0; j < l; j++)
+			{
+				n *= 2;
+			}
+			sum += k * n;
+			n = 1;
+		}
+		l++;
+	}
+	return (sum);
+}
+
