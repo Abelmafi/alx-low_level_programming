bb951b45a7c50dce1ffbff567afb6a65794f33ff
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-23 16:47:52 +0300

    commited

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,44 +13,36 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l = 0;
+	int l = 0, size;
 	int a = strlen(n1);
 	int b = strlen(n2);
-	int size;
 
-	size = ((a >= b) ? (a + 1) : (b + 1));
-	if (!*n1 && !*n2)
-	{
+	if (size_r < a + 1 || size_r < b + 1)
 		return (0);
-	}
-	if ((a + 2) > size_r || (b + 2) > size_r)
+	while (a > 0 || b > 0)
 	{
-		return (0);
-	}
-	else
-	{
-		while (a > 0 || b > 0)
+		int f = (int)((a > 0) ? (n1[a - 1] - '0') : 0);
+		int g = (int)((b > 0) ? (n2[b - 1] - '0') : 0);
+		int n = f + g + l;
+		int m = n % 10;
+
+		if (n > 9)
 		{
-			int f = (int)((a > 0) ? (n1[a - 1] - '0') : 0);
-			int g = (int)((b > 0) ? (n2[b - 1] - '0') : 0);
-			int n = f + g + l;
-			int m = n % 10;
-
-			if (n > 9)
-			{
-				r[size - 1] = (char)(m + '0');
-				l = 1;
-			}
-			else
-			{	r[size - 1] = (char)(n + '0');
-				l = 0;
-			}
-			size--;
-			a--;
-			b--;
+			r[size_r - 1] = (char)(m + '0');
+			l = 1;
 		}
-		if (l == 1)
-			r[0] = 1 + '0';
+		else
+		{
+			r[size_r - 1] = (char)(n + '0');
+			l = 0;
+		}
+		size_r--;
+		a--;
+		b--;
 	}
-	return (r);
+	if (size_r == 1 && l == 1)
+		return (0);
+	if (size_r > 1 && l == 1)
+		r[size_r - 1] = 1 + '0';
+	return (r + size_r - 1);
 }
