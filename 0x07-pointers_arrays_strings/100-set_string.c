eb0c306fe22463d4d19cae6ae1a5634f73ab7236
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-23 15:32:06 +0300

    commited

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - swap pointer adress.
+ * @s: frist string.
+ * @to: second string.
+ *
+ * Return: Nothing.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
