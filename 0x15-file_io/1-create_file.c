68dbe7ad0c92bc57c94278e57e771cb0aadae3de
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-25 19:21:52 +0300

    bug fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,15 +12,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wrt, len;
 
 	if (text_content == NULL)
 		text_content = "";
-	fd = creat(filename, 00600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (filename == NULL)
 		return (-1);
-	write(fd, text_content, sizeof(text_content));
+	for (len = 0; text_content[len]; len++)
+	{}
+	wrt = write(fd, text_content, len);
+	if (wrt == -1)
+		return (-1);
 	return (1);
 }
