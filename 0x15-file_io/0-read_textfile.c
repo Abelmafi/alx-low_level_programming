96344d5f56d41ab13073b85929d98346d72effb1
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-25 17:07:17 +0300

    commited

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - read file.
+ * @filename: file name.
+ * @letters: print char size.
+ *
+ * Return: number of printed chars.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, sz, len;
+	char *c = (char *) malloc(letters * sizeof(char) + 1);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	sz = read(fd, c, letters);
+	if (sz < 0 || sz == NULL)
+		return (0);
+	c[sz] = '\0';
+	printf("%d %s\n", fd, c);
+	return (sz);
+}
