9db61f09803b61ed9a9d4aa80ec194f6e500043d
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-26 11:07:01 +0300

    bug fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -55,7 +55,7 @@ int main(int ac, char **av)
 	res = cp_file(av[1], av[2]);
 	if (res == 98)
 	{
-		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE");
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (res == 99)
