9a3459b6353bc195103823a2baa7851a8f2a19fc
Abel Mesfin <abelmesfin555@gmail.com>
2022-07-26 10:40:05 +0300

    bug fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,13 +20,15 @@ int cp_file(char *sourse, char *target)
 
 	fd1 = open(sourse, O_RDONLY);
 	fd2 = open(target, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	rd = read(fd1, buffer, 1024);
+	while ((rd = read(fd1, buffer, 1024)) > 0)
+	{
 	if (rd == -1)
 		return (98);
 	if (rd > 0)
 		wr = write(fd2, buffer, rd);
 	if (wr == -1)
 		return (98);
+	}
 	close(fd1);
 	close(fd2);
 	return (1);
