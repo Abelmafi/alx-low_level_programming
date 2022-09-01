5a75127ae8534c154e697f88fea023639d2b6590
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2022-09-01 14:03:50 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index){
 	dlistint_t *del_node = *head;
 	unsigned int n = 0;
 
-	if (*head == NULL || index < 0)
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
