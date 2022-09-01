94edf34ca632263bb7558e8e8b3d6215135bd466
abel mesfin <abelmesfin555@gmail.com>
2022-09-01 16:30:40 +0300

    bug fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,11 +23,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	if (idx == 0)
 	{
-		new->next = *h;
-		new->prev = NULL;
 		if (*h)
-			(*h)->prev = new;
+			current->prev = new;
 		*h = new;
+		new->next = current;
+                new->prev = NULL;
+		return (new);
 	}
 	idx--;
 	while (num < idx && current)
