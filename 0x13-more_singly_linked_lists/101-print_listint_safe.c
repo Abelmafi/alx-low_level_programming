82d9e11a15ee578f7e207563d2aeea64b660a758
abel mesfin <abelmesfin555@gmail.com>
2022-09-04 17:53:12 +0300

    bugfix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,7 +11,7 @@ size_t print_listint_safe(const listint_t *head)
 	int n = 0;
 
 	if (head == NULL)
-		exit(98);
+		return (0);
 	slow = slow->next;
 	fast = fast->next->next;
 	while (fast && fast->next)
