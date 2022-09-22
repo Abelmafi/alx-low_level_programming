02042c89e78c6e77f00c11cd8f3726d2603e7a71
abel mesfin <abelmesfin555@gmail.com>
2022-09-22 22:59:19 +0300

    commited

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *t;
 
 	t = malloc(sizeof(hash_table_t));
+	t->size = size;
 	t->array = malloc(sizeof(hash_node_t) * size);
 
 	return (t);
