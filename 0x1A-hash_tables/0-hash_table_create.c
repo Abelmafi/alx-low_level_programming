787c486b1d9459893bfce128915bf4fcbb6cf3fe
abel mesfin <abelmesfin555@gmail.com>
2022-09-22 22:51:39 +0300

    commited

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hash table
+ * @size: size of hash table
+ *
+ * Return: newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *t;
+
+	t = malloc(sizeof(hash_table_t));
+	t->array = malloc(sizeof(hash_node_t) * size);
+
+	return t;
+}
