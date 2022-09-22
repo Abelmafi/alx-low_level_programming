b239d21d6a869d3589defb5d2752910fc547cb74
abel mesfin <abelmesfin555@gmail.com>
2022-09-23 00:18:30 +0300

    output error fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,5 +28,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index])
 		new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+	free(new_node);
 	return (1);
 }
