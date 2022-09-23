8d183dfad1c8b30ca8a93da6a94997e7b41eae36
abel mesfin <abelmesfin555@gmail.com>
2022-09-23 08:38:41 +0300

    output error fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,11 +21,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	index = key_index((unsigned char *)key, ht->size);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	
+
 	return (1);
 }
