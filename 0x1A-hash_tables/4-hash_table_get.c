bc14e995d623ef3155aa19a05a796c992bcc10c2
abel mesfin <abelmesfin555@gmail.com>
2022-09-23 10:03:47 +0300

    inital commit

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - retrives a value associated with given key.
+ * @ht: hash table.
+ * @key: key element.
+ *
+ * Return: value associated with key or NULL if its not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
