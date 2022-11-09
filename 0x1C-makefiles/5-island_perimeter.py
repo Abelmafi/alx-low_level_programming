0fcfb7d4016caed0c15587c202a2d477e61879af
abel mesfin <abelmesfin555@gmail.com>
2022-11-09 23:32:04 +0300

    inital commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,15 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i - 1 >= 0 and grid[i - 1][j] == 0:
-                    p += 1
-                if j + 1 < len(grid[i]) and grid[i][j + 1] == 0:
-                    p += 1
-                if j - 1 >= 0 and grid[i][j - 1] == 0:
-                    p += 1
-                if i + 1 < len(grid) and grid[i + 1][j] == 0:
-                    p += 1
+                try:
+                    if grid[i - 1][j] == 0:
+                        p += 1
+                    if grid[i][j + 1] == 0:
+                        p += 1
+                    if grid[i][j - 1] == 0:
+                        p += 1
+                    if grid[i + 1][j] == 0:
+                        p += 1
+                except IndexError:
+                        p += 1
     return (p)
