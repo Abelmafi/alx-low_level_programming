e5bb8bf34b95e523f6e82723d1a376e739a2b0a7
Abel <abelmesfin555@gmail.com>
2023-01-04 02:27:26 +0300

    output error fix

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -33,7 +33,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		front = front->next;
 	while (front->n < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", front->index, front->n);
+		printf("Value checked at index [%ld] = [%d]\n", front->index, front->n);
 		prev_index = step;
 		prev = front;
 		step += sqrt(size);
@@ -46,19 +46,19 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		for (i = 0; i < sqrt(size); i++)
 			front = front->next;
 	}
-	printf("Value checked array[%ld] = [%d]\n", front->index, front->n);
+	printf("Value checked at index [%ld] = [%d]\n", front->index, front->n);
 	printf("Value found between indexes [%ld] and", prev_index);
 	printf(" [%ld]\n", minn(step, size - 1));
 	while (prev_index < minn(step, size - 1) && prev->n < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", prev->index, prev->n);
+		printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
 		if (prev->next)
 			prev = prev->next;
 		prev_index++;
 	}
 	if (prev->n == value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", prev->index, prev->n);
+		printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
 		return (prev);
 	}
 	return (NULL);
