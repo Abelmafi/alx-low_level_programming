c445a25fdb0ef7ad4da5191dd9be07cadc798ef6
Abelmafi <105563023+Abelmafi@users.noreply.github.com>
2023-08-28 21:43:16 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,11 @@ char *str_concat(char *s1, char *s2)
 		for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
 	}
+	if (s1 == NULL)
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		s[i] = s2[i];
+	}
 	else
 	{
 		for (i = 0; s1[i] != '\0'; i++)
